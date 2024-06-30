96025f03ea9445e2cf7f46b7fb7db89b4b269a87
Nathan David Hill <nathan.hill@gmx.at>
2024-06-29 20:56:45 -0400

    allocating the method into a seperate class so I can scale this solution and add more small projects to it

--- LCP/Program.cs
+++ LCP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LCP;
 
 namespace LeetCodePractice
 {
@@ -9,27 +10,12 @@ namespace LeetCodePractice
             int[] nums = new int[] { 3, 3 };
             int target = 6;
 
-            int[] data = TwoSum(nums, target);
+            int[] data = TwoSum.sumUp(nums, target);
             foreach (var i in data)
             {
                 Console.WriteLine(i);
             }
         }
 
-        public static int[] TwoSum(int[] nums, int target)
-        {
-            for (int i = 0; i < nums.Length; i++)
-            {
-                for (int j = 0; j < nums.Length; j++)
-                {
-                    if (nums[i] + nums[j] == target && i != j)
-                    {
-                        return new int[] { i, j };
-                    }
-                }
-            }
-
-            return null;
-        }
     }
 }
---
+++ LCP/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCP
+{
+    public class TwoSum
+    {
+        public static int[] sumUp(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target && i != j)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
