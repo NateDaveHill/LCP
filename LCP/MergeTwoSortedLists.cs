6537acde6bae8a5beaf81ebb1be8604b34e58c02
Nathan David Hill <nathan.hill@gmx.at>
2024-07-01 12:48:54 -0400

    trying to set up MergeTwoSortedLists with ListeNode as shown in on the LeetCode page

---
+++ LCP/MergeTwoSortedLists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCP
+{
+    public class MergeTwoSortedLists
+    {
+        public MergeTwoSortedLists()
+        {
+            ListNode node1 = new ListNode(1);
+            ListNode node2 = new ListNode(2);
+            ListNode node3 = new ListNode(3);
+            ListNode node4 = new ListNode(4);
+
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+
+            ListNode list1 = node1;
+            ListNode list2 = node2;
+        }
+
+
+        public ListNode MergeTwoSortedLists((ListNode list1, ListNode list2)
+        {
+        }
+    }
+}
