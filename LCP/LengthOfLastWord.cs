3509ee1e1ff2d1a1867bb5ccb2bff75ebd6502bd
Nathan David Hill <nathan.hill@gmx.at>
2024-07-11 18:55:13 -0400

    LeetCode Challenge to return the length of the last word in a list of strings

---
+++ LCP/LengthOfLastWord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCP
+{
+    public class LengthOfLastWord
+    {
+        public int LengthOfLastWords(string s)
+        {
+            List<string> words = new List<string>();
+            List<string> tempWords = new List<string>();
+            words = s.Split(' ').ToList();
+
+            foreach (var word in words)
+            {
+                if (word != "")
+                {
+                    tempWords.Add(word);
+                }
+            }
+
+            return tempWords[tempWords.Count - 1].Length;
+        }
+    }
+}
