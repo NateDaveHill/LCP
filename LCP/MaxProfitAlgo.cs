1fa5c61ecb684cc5e8d98141c6a9913f96fc2b15
Nathan David Hill <nathan.hill@gmx.at>
2024-07-11 19:13:24 -0400

    I got it to work with my case, but not every single testcase... ChatGpt helped optimze the code in a way that covers all testcases

---
+++ LCP/MaxProfitAlgo.cs
@@ -0,0 +1,56 @@
+﻿namespace LCP;
+
+public class MaxProfitAlgo
+{
+    public int MaxProfit(int[] prices)
+    {
+        //List<int> buyingValue = new List<int>();
+        //IList<int> sellingValue = new List<int>();
+        //int count = 0;
+
+
+        //for (int i = 0; i < prices.Length; i++)
+        //{
+        //    count++;
+
+        //    for (int j = i + 1; j < prices.Length; j++)
+        //    {
+        //        if (prices[i] < prices[j])
+        //        {
+        //            buyingValue.Add(prices[i]);
+        //        }
+        //        else
+        //        {
+        //            sellingValue.Add(prices[i]);
+        //        }
+
+        //        if (buyingValue.Count == 0)
+        //        {
+        //            sellingValue.Clear();
+
+        //        }
+        //    }
+        //}
+
+        //try
+        //{
+        //    return sellingValue.Max() - buyingValue.Min();
+        //}
+        //catch (Exception e)
+        //{
+        //    return 0;
+        //}
+
+
+        if (prices == null || prices.Length == 0) return 0;
+        int minPrice = int.MaxValue, maxProfit = 0;
+
+        foreach (var price in prices)
+        {
+            if (price < minPrice) minPrice = price;
+            if (price - minPrice > maxProfit) maxProfit = price - minPrice;
+        }
+
+        return maxProfit;
+    }
+}
--- LCP/Program.cs
+++ LCP/Program.cs
@@ -7,7 +7,6 @@ namespace LeetCodePractice
     {
         static void Main(string[] args)
         {
-
             //PalindromeNumber palindromeNumber = new PalindromeNumber();
             //TwoSum twoSum = new TwoSum();
             //MergeTwoSortedLists merge = new MergeTwoSortedLists();
@@ -27,31 +26,40 @@ namespace LeetCodePractice
             //bool test = palindromeNumber.checkIfPalindrome(1234);
             //Console.WriteLine(test);
 
-        //------------------------------------------------------
-        //ListNode node1 = new ListNode(1);
-        //ListNode node2 = new ListNode(2);
-        //ListNode node3 = new ListNode(3);
-        //ListNode node4 = new ListNode(4);
+            //------------------------------------------------------
+            //ListNode node1 = new ListNode(1);
+            //ListNode node2 = new ListNode(2);
+            //ListNode node3 = new ListNode(3);
+            //ListNode node4 = new ListNode(4);
 
-        //node1.next = node2;
-        //node2.next = node3;
-        //node3.next = node4;
+            //node1.next = node2;
+            //node2.next = node3;
+            //node3.next = node4;
 
-        //ListNode list1 = node1;
-        //ListNode list2 = node2;
+            //ListNode list1 = node1;
+            //ListNode list2 = node2;
 
-        //ListNode newList = merge.mergeTwoSortedLists(list1, list2);
-        //Console.ReadKey();
+            //ListNode newList = merge.mergeTwoSortedLists(list1, list2);
+            //Console.ReadKey();
 
-        //------------------------------------------------------
-        string s;
+            //------------------------------------------------------
+            //string s;
 
-            //s = "Hello World";
-            //s = "   fly me   to   the moon  ";
-            s = "luffy is still joyboy";
+            ////s = "Hello World";
+            ////s = "   fly me   to   the moon  ";
+            //s = "luffy is still joyboy";
 
-            LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
-        Console.WriteLine(lengthOfLastWord.LengthOfLastWords(s));
+            //LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
+            //Console.WriteLine(lengthOfLastWord.LengthOfLastWords(s));
+
+            //------------------------------------------------------
+            int[] prices = new int[] { 1,2 };
+            //int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+            //int[] prices = new int[] { 7, 6, 4, 3, 1 };
+            MaxProfitAlgo maxProfitAlgo = new MaxProfitAlgo();
+
+            int test = maxProfitAlgo.MaxProfit(prices);
+            Console.WriteLine(test);
 
 
         }
