5dc5211144a4adebb246636f1fe76d12423d2a28
Nathan David Hill <nathan.hill@gmx.at>
2024-07-14 19:13:40 -0400

    learning process from another leetcoder

--- LCP/ContainsDuplicates.cs
+++ LCP/ContainsDuplicates.cs
@@ -4,21 +4,34 @@ public class ContainsDuplicates
 {
     public bool ContainsDuplicate(int[] nums)
     {
-        
-        for (int i = 0; i < nums.Length; i++)
-        {
-            for (int j = 1; j < nums.Length; j++)
-            {
-                if (i != j)
-                {
-                    if (nums[i] == nums[j])
-                    {
-                        return true;
-                    }
-                }
-            }
-        }
 
+        //for (int i = 0; i < nums.Length; i++)
+        //{
+        //    for (int j = 1; j < nums.Length; j++)
+        //    {
+        //        if (i != j)
+        //        {
+        //            if (nums[i] == nums[j])
+        //            {
+        //                return true;
+        //            }
+        //        }
+        //    }
+        //}
+
+        //return false;
+
+
+        //someone elses solution to help the time out error when a huge data set is being given
+        Dictionary<int, bool> d = new Dictionary<int, bool>();
+
+        foreach (int x in nums)
+        {
+            if (d.ContainsKey(x))
+                return true;
+            else
+                d[x] = false;
+        }
         return false;
     }
 }
